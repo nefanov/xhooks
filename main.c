f31705e2c37d068f6b06480f512bbb04d6427b5c
nefanov <nefanov90@gmail.com>
2016-10-30 22:57:04 +0300

    Deleting unused includes

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <X11/Xatom.h>
-#include <X11/keysym.h>
+
 
 /* this function replaces an old symbol in the X keyboard map with the new one */
 int chande_sym(const char* old,const char* new) {
@@ -13,7 +11,6 @@ int chande_sym(const char* old,const char* new) {
     }
 
     int	nF2Keycode;
-
     KeySym oldsym = XStringToKeysym (old);
     KeySym symbol = XStringToKeysym (new);
     nF2Keycode = XKeysymToKeycode (display, oldsym);
