76714a5ee35f16893a2a2964c6e7d811b8398aa0
nefanov <nefanov90@gmail.com>
2016-11-02 21:20:25 +0300

    JNI correct wrapped cpp (now fixed).

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ -B "/usr/lib/jvm/java-8-openjdk-amd64/include" -shared -lib  -o libxrep.so -fPIC -c impl.cpp -lxcb -lX11
+g++ -I/usr/lib/jvm/java-8-openjdk-amd64/include -I/usr/lib/jvm/java-8-openjdk-amd64/include/linux -shared -lib  -o libxrep.so -fPIC -c impl.cpp -lxcb -lX11
